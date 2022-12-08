1c3514f66b40f6b20ccf3059db20de508f993cfd
ESTANISLAO ALIAGA KUZYK <esalku@floridauniversitaria.es>
2022-12-08 16:03:04 +0100

    Creando clases principales.

---
+++ AEV2PSP/src/acts/Hilos.java
@@ -0,0 +1,11 @@
+package acts;
+
+public class Hilos implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ AEV2PSP/src/acts/Servidor.java
@@ -0,0 +1,10 @@
+package acts;
+
+public class Servidor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
