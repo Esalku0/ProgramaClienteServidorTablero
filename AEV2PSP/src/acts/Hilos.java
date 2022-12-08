e3083da5e0de6419fc93a1f3f945986c70144933
ESTANISLAO ALIAGA KUZYK <esalku@floridauniversitaria.es>
2022-12-08 16:10:44 +0100

    Agregando botones del tablero al apartado cliente.

--- AEV2PSP/src/acts/Hilos.java
+++ AEV2PSP/src/acts/Hilos.java
@@ -1,10 +1,18 @@
 package acts;
 
+import java.net.Socket;
+
 public class Hilos implements Runnable {
+	
+	Socket conexion;
+
+	public Hilos(Socket conexion) {
+		this.conexion=conexion;
+	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
--- AEV2PSP/src/acts/Servidor.java
+++ AEV2PSP/src/acts/Servidor.java
@@ -1,10 +1,28 @@
 package acts;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+
 public class Servidor {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		ServerSocket socketEscucha = null;
+		System.err.println("CONEXION FUNCIONAL");
+		try {
+			socketEscucha = new ServerSocket(7878);
+		} catch (IOException e) {
+			System.err.println("SERVIDOR >>> Error");
+			return;
+		}
+	
+			Socket conexion = socketEscucha.accept();
+			System.err.println("SERVIDOR >>> Conexion recibida --> Lanza hilo clase Peticion");
+			Hilos  p = new Hilos(conexion);
+			Thread hilo = new Thread(p);
+			hilo.start();
+		
 	}
 
 }
